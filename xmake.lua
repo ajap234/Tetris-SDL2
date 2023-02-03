b4cc4dcb115434b63789feebe25837a6bc2066e6
aJap234 <alexymansuy31@gmail.com>
2023-02-03 09:40:33 +0100

    Fixing xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,10 @@
 add_requires("libsdl 2", "libsdl_image 2", "libsdl_ttf >= 2.0.18", "libsdl_mixer 2")
-target("console")
+add_requires("libsdl 2", { configs = { shared = true } })
+target("tetris")
 	set_kind("binary")
 	add_packages("libsdl", "libsdl_image", "libsdl_ttf", "libsdl_mixer")
+<<<<<<< HEAD
 	add_files("src/*.cpp", {includedirs = "inc"})
-	add_linkdirs("./")
+=======
+	add_files("src/*.cpp", {includedirs = "inc", languages = "c++17"})
+>>>>>>> ff5e75e (Fixing xmake.lua)
